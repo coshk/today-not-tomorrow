0ab8cd53f8e3605b5a557e47313813454f0295d3
Ivan Kotov <iamivankotov@gmail.com>
2022-11-14 04:26:13 +0000

    App arrow-functioned

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import { Header } from "./components/Header/Header";
-import { Main } from "./components/Main/Main";
-import "./App.css";
+import React, {FC} from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-function App() {
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+import './App.css';
+
+
+const App: FC = () => {
   return (
     <Provider store={store}>
-      <div className='App'>
+      <div className="App">
         <Header />
         <Main />
       </div>
